8ba7b0fd0dea99b12d77778b440e2a62681f03a2
markvong-o <83755325+markvong-o@users.noreply.github.com>
2022-06-22 16:50:56 -0700

    Pls

--- okta_multidemo/config.py
+++ okta_multidemo/config.py
@@ -5,13 +5,14 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 # TODO: is OKTA_ALT_ENV used anymore? if not, remove below
-alt_env = os.getenv('OKTA_ALT_ENV', None)
-if alt_env:
-    env_path = Path('.') / alt_env
-    load_dotenv(dotenv_path=env_path, override=True)
-else:
-    load_dotenv()
+# alt_env = os.getenv('OKTA_ALT_ENV', None)
+# if alt_env:
+#     env_path = Path('.') / alt_env
+#     load_dotenv(dotenv_path=env_path, override=True)
+# else:
+#     load_dotenv()
 
+# ENV="production"
 
 class BaseConfig(object):
     DEBUG = False
@@ -21,9 +22,9 @@ class BaseConfig(object):
     CACHE_DIR = '/tmp'
     CACHE_DEFAULT_TIMEOUT = 300
     DB_PATH = os.getenv('DB_PATH')
-    OKTA_SCOPES = os.getenv('OKTA_SCOPES').split(',')
-    OKTA_ADMIN_SCOPES = os.getenv('OKTA_ADMIN_SCOPES').split(',')
-    OKTA_O4O_SCOPES = os.getenv('OKTA_O4O_SCOPES').split(',')
+    # OKTA_SCOPES = os.getenv('OKTA_SCOPES').split(',')
+    # OKTA_ADMIN_SCOPES = os.getenv('OKTA_ADMIN_SCOPES').split(',')
+    # OKTA_O4O_SCOPES = os.getenv('OKTA_O4O_SCOPES').split(',')
     UDP_CONFIG_URL = os.getenv('UDP_CONFIG_URL')
     UDP_ISSUER = os.getenv('UDP_ISSUER')
     UDP_CLIENT_ID = os.getenv('UDP_CLIENT_ID')
