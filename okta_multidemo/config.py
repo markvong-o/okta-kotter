0b72fec7526bfb0e0a05e709d6705c7d45e913d8
markvong-o <83755325+markvong-o@users.noreply.github.com>
2022-06-22 16:39:02 -0700

    Updates?

--- okta_multidemo/config.py
+++ okta_multidemo/config.py
@@ -3,14 +3,14 @@ import os
 from pathlib import Path
 
 
-# from dotenv import load_dotenv
-# # TODO: is OKTA_ALT_ENV used anymore? if not, remove below
-# alt_env = os.getenv('OKTA_ALT_ENV', None)
-# if alt_env:
-#     env_path = Path('.') / alt_env
-#     load_dotenv(dotenv_path=env_path, override=True)
-# else:
-#     load_dotenv()
+from dotenv import load_dotenv
+# TODO: is OKTA_ALT_ENV used anymore? if not, remove below
+alt_env = os.getenv('OKTA_ALT_ENV', None)
+if alt_env:
+    env_path = Path('.') / alt_env
+    load_dotenv(dotenv_path=env_path, override=True)
+else:
+    load_dotenv()
 
 
 class BaseConfig(object):
